54e98b0144cdb74939a7e07164a604aa139030ca
Ariwira <ariwiranugraha787@gmail.com>
2023-11-20 13:48:41 +0800

    struktur folder

---
+++ webpack.prod.js
@@ -0,0 +1,27 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
+});
