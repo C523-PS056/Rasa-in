225d78197e47a1e8074cffe05ca57c4d0f85d08d
Ariwira <ariwiranugraha787@gmail.com>
2023-11-21 16:22:05 +0800

    tambah header footer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,9 @@
 import 'regenerator-runtime';
 import '../styles/style.css';
+import App from './views/app';
+
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
